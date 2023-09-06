55e4d1a0005aa9de9db21113d89299e412be2e77
kian <lordkian2000@gmail.com>
2023-09-06 16:27:04 +0200

    added needed info for tele bot to Setting

--- Mensa-Marburg/Setting.cs
+++ Mensa-Marburg/Setting.cs
@@ -2,5 +2,8 @@ namespace Mensa_Marburg;
 
 public class Setting
 {
-    
+    // I cant commit Telegram Bot Token of test bot
+    public string TelegramBotToken { get; set; }
+    public List<long> AdminsID { get; set; }
+    public long ChannelID { get; set; }
 }
