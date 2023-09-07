e307108daf3d3c894dad7ffcf31ef2a711abdb61
kian <lordkian2000@gmail.com>
2023-09-07 18:04:38 +0200

    Added Dictionarys to SpeiseContainer.cs

--- Mensa-Marburg/Data/SpeiseContainer.cs
+++ Mensa-Marburg/Data/SpeiseContainer.cs
@@ -7,5 +7,8 @@ public class SpeiseContainer
     public List<Gericht> Gerichte { get; set; }
     public List<Gericht> ParsedGerichte { get; set; }
     public List<Beilage> Beilagen { get; set; }
+    public Dictionary<string, string> EssenTypeDic { get; set; }
+    public Dictionary<string, string> MensaDic { get; set; }
+    
     
 }
