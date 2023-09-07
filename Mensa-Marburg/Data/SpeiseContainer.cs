d45f631554edd695e0c0b08eeeb09b5b3784c6c4
kian <lordkian2000@gmail.com>
2023-09-07 23:22:50 +0200

    private set for props

--- Mensa-Marburg/Data/SpeiseContainer.cs
+++ Mensa-Marburg/Data/SpeiseContainer.cs
@@ -4,11 +4,11 @@ namespace Mensa_Marburg.Data;
 
 public class SpeiseContainer
 {
-    public List<Gericht> Gerichte { get; set; }
-    public List<Gericht> ParsedGerichte { get; set; }
-    public List<Beilage> Beilagen { get; set; }
-    public Dictionary<string, string> EssenTypeDic { get; set; }
-    public Dictionary<string, string> MensaDic { get; set; }
-    
-    
+    public List<Gericht> Gerichte { get; private set; }
+    public List<Gericht> ParsedGerichte { get; private set; }
+    public List<Beilage> Beilagen { get; private set; }
+    public Dictionary<string, string> EssenTypeDic { get; private set; }
+    public Dictionary<string, string> MensaDic { get; private set; }
+
+   
 }
