cd47e16a3bd2f9655bccc724d8918901193b61fc
kian <lordkian2000@gmail.com>
2023-09-07 17:59:09 +0200

    update Gericht

--- Mensa-Marburg/Data/DataType/Gericht.cs
+++ Mensa-Marburg/Data/DataType/Gericht.cs
@@ -1,13 +1,23 @@
 namespace Mensa_Marburg.Data.DataType;
 
-public class Gericht: Speise
+public class Gericht : Speise
 {
     public string EssenType { get; set; }
     public string Date { get; set; }
+    public string Kosten { get; set; }
+    public List<SubGericht> SubGerichte { get; set; }
+
+    public Gericht()
+    {
+        SubGerichte = new List<SubGericht>();
+    }
+    
+    public string HashString => $"{EssenType}{Date}{Name}";
+}
+
+public class SubGericht
+{
     public string Mensa { get; set; }
     public string Type2 { get; set; }
     public string MenuArt { get; set; }
-    public string Kosten { get; set; }
-    
-    public string HashString => $"{EssenType}{Date}{Name}";
 }
