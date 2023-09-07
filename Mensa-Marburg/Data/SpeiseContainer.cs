21dd17598c55887e985669f9ea8786a23c9e6f38
kian <lordkian2000@gmail.com>
2023-09-07 23:23:47 +0200

    DownloadData() added loads and load Dics

--- Mensa-Marburg/Data/SpeiseContainer.cs
+++ Mensa-Marburg/Data/SpeiseContainer.cs
@@ -1,3 +1,4 @@
+using HtmlAgilityPack;
 using Mensa_Marburg.Data.DataType;
 
 namespace Mensa_Marburg.Data;
@@ -10,5 +11,24 @@ public class SpeiseContainer
     public Dictionary<string, string> EssenTypeDic { get; private set; }
     public Dictionary<string, string> MensaDic { get; private set; }
 
-   
+    public void DownloadData()
+    {
+        // load props
+        Gerichte = new List<Gericht>();
+        ParsedGerichte = new List<Gericht>();
+        Beilagen = new List<Beilage>();
+        EssenTypeDic = new Dictionary<string, string>();
+        MensaDic = new Dictionary<string, string>();
+        // load function vars
+        var loader = new HtmlWeb();
+        var doc = loader.Load(Setting.Instance.BaseURL);
+        // load Dics
+        foreach (var item in doc.DocumentNode.SelectNodes("//select[@class='neo-menu-single-filter-type']/option"))
+            if (item.Attributes.Contains("value"))
+                EssenTypeDic[item.Attributes["value"].Value.Trim()] = item.InnerText.Replace(" / ", "_").Trim();
+        foreach (var item in doc.DocumentNode.SelectNodes("//select[@class='neo-menu-single-canteens']/option"))
+            if (item.Attributes.Contains("value"))
+                MensaDic[item.Attributes["value"].Value.Trim()] = item.InnerText.Trim();
+       
+    }
 }
