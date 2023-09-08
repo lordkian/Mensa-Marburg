d20569f3b9eee9bc1c36449259abdc49933f6fc5
kian <lordkian2000@gmail.com>
2023-09-09 00:03:26 +0200

    Added required methods

--- Mensa-Marburg/TelegramBot.cs
+++ Mensa-Marburg/TelegramBot.cs
@@ -20,12 +20,49 @@ public class TelegramBot
             AllowedUpdates = Array.Empty<UpdateType>()
         };
 
-        // botClient.StartReceiving(
-        //     updateHandler: HandleUpdateAsync,
-        //     pollingErrorHandler: HandlePollingErrorAsync,
-        //     receiverOptions: receiverOptions,
-        //     cancellationToken: cts.Token
-        // );
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
     }
-    
+    private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        if (update.Message is not { } message)
+            return;
+        // Only process text messages
+        if (message.Text is not { } messageText)
+            return;
+        var chatId = message.Chat.Id;
+        if (Setting.Instance.AdminsID.Contains(chatId))
+        {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+        else
+        {
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "unauthorized access\nyour chatID: " + chatId,
+                cancellationToken: cancellationToken);
+        }
+    }
+
+    private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        return Task.CompletedTask;
+    }
 }
