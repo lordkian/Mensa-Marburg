9779d52bd3c5d9f6d55e03dfdf8a7933f5d563ac
kian <lordkian2000@gmail.com>
2023-09-08 16:19:34 +0200

    Add load Gerichte

--- Mensa-Marburg/Data/SpeiseContainer.cs
+++ Mensa-Marburg/Data/SpeiseContainer.cs
@@ -29,6 +29,44 @@ public class SpeiseContainer
         foreach (var item in doc.DocumentNode.SelectNodes("//select[@class='neo-menu-single-canteens']/option"))
             if (item.Attributes.Contains("value"))
                 MensaDic[item.Attributes["value"].Value.Trim()] = item.InnerText.Trim();
-       
+        // load Gerichte
+        foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='neo-menu-single-dishes']//tr[not(@data-day)]"))
+        {
+            var gericht = new Gericht();
+            gericht.SubGerichte.Add(new SubGericht());
+            try
+            {
+                gericht.SubGerichte[0].Type2 = item.Attributes["data-type2"].Value.Trim();
+            }
+            catch (Exception e)
+            {
+                gericht.SubGerichte[0].Type2 = "";
+            }
+
+            var essenTypeTmp = item.Attributes["data-type"].Value.Trim();
+            if (essenTypeTmp.Contains(" "))
+                essenTypeTmp.Split(" ").ToList().ForEach(s => { gericht.EssenType += " / " + EssenTypeDic[s]; });
+            else
+                gericht.EssenType = EssenTypeDic[essenTypeTmp];
+
+
+            gericht.SubGerichte[0].Mensa = MensaDic[item.Attributes["data-canteen"].Value.Trim()];
+            gericht.Date = item.Attributes["data-date"].Value.Trim();
+
+            gericht.Kosten = item.SelectSingleNode(".//td[@class=\"neo-menu-single-price\"]/span").InnerText.Trim();
+            gericht.SubGerichte[0].MenuArt =
+                item.SelectSingleNode(".//span[@class=\"neo-menu-single-type\"]").InnerText.Trim();
+
+            var node = item.SelectSingleNode(".//span[@class=\"neo-menu-single-title\"]");
+            gericht.Name = node.InnerText.Trim();
+            var kennzeichnungenNode = node.SelectNodes(".//abbr");
+            if ( kennzeichnungenNode is { Count: > 0 })
+                foreach (var item2 in kennzeichnungenNode)
+                {
+                    gericht.Kennzeichnungen.TryAdd(item2.InnerText, item2.Attributes["title"].Value.Trim());
+                }
+
+            Gerichte.Add(gericht);
+        }
     }
 }
