7fe177aea367fb0b1ba85dd6485228170c644e5b
kian <lordkian2000@gmail.com>
2023-09-14 22:31:26 +0200

    Added ListAdmin AddAdmin and RemoveAdmin to TelegramBot

--- Mensa-Marburg/TelegramBot.cs
+++ Mensa-Marburg/TelegramBot.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
@@ -12,6 +13,8 @@ public class TelegramBot
     private TelegramBotClient _telegramBotClient;
     private CancellationTokenSource cts;
     private Dictionary<string, Action<ITelegramBotClient, Update, CancellationToken>> dictionary;
+    private Action<ITelegramBotClient, Update, CancellationToken, string>? nonKeyboardAction;
+    private Regex TelegramID = new Regex("^(-)?\\d{6,11}$");
 
     public TelegramBot()
     {
@@ -25,7 +28,10 @@ public class TelegramBot
         dictionary = new Dictionary<string, Action<ITelegramBotClient, Update, CancellationToken>>()
         {
             ["/start"] = Start,
-            ["Admin"] = Admin
+            ["Admin"] = Admin,
+            ["Add Admin"] = AddAdmin,
+            ["Remove Admin"] = RemoveAdmin,
+            ["List Admin"] = ListAdmin
         };
 
         _telegramBotClient.StartReceiving(
@@ -111,6 +117,99 @@ public class TelegramBot
             text: "Admin Menu:",
             replyMarkup: replyKeyboardMarkup,
             cancellationToken: cancellationToken);
+    }
+
+    private void AddAdmin(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Enter new Admins ID",
+            replyMarkup: new ReplyKeyboardRemove(),
+            cancellationToken: cancellationToken);
+        nonKeyboardAction = (botClient, update, cancellationToken, messageText) =>
+        {
+            if (TelegramID.IsMatch(messageText))
+            {
+                Setting.Instance.AdminsIDs.Add(long.Parse(messageText));
+                Setting.SaveSetting();
+                nonKeyboardAction = null;
+                botClient.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    text: "Done",
+                    cancellationToken: cancellationToken);
+                Start(botClient, update, cancellationToken);
+            }
+            else
+            {
+                botClient.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    text: "The new user Id is invalid",
+                    cancellationToken: cancellationToken);
+            }
+        };
+    }
+
+    private void ListAdmin(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        var replyKeyboardMarkup = new ReplyKeyboardMarkup(new[]
+        {
+            new KeyboardButton[] { "List Admin" },
+            new KeyboardButton[] { "Add Admin" },
+            new KeyboardButton[] { "Remove Admin" },
+            new KeyboardButton[] { "Set Channel ID" },
+        })
+        {
+            ResizeKeyboard = true
+        };
+        botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "List of Admins:\n" + string.Join(", ", Setting.Instance.AdminsIDs),
+            replyMarkup: replyKeyboardMarkup,
+            cancellationToken: cancellationToken);
+    }
+
+    private void RemoveAdmin(ITelegramBotClient botClient, Update update,
+        CancellationToken cancellationToken)
+    {
+        botClient.SendTextMessageAsync(
+            chatId: update.Message.Chat.Id,
+            text: "Enter Admins ID to remove:\n" + string.Join(", ", Setting.Instance.AdminsIDs),
+            replyMarkup: new ReplyKeyboardRemove(),
+            cancellationToken: cancellationToken);
+        nonKeyboardAction = (botClient, update, cancellationToken, messageText) =>
+        {
+            if (TelegramID.IsMatch(messageText))
+            {
+                var id = long.Parse(messageText);
+                if (Setting.Instance.AdminsIDs.Contains(id))
+                {
+                    Setting.Instance.AdminsIDs.Remove(id);
+                    Setting.SaveSetting();
+                    nonKeyboardAction = null;
+                    botClient.SendTextMessageAsync(
+                        chatId: update.Message.Chat.Id,
+                        text: "Done",
+                        cancellationToken: cancellationToken);
+                    Start(botClient, update, cancellationToken);
+                }
+                else
+                {
+                    botClient.SendTextMessageAsync(
+                        chatId: update.Message.Chat.Id,
+                        text: "The user is not Admin",
+                        cancellationToken: cancellationToken);
+                }
+            }
+            else
+            {
+                botClient.SendTextMessageAsync(
+                    chatId: update.Message.Chat.Id,
+                    text: "The user Id is invalid",
+                    cancellationToken: cancellationToken);
+            }
+        };
     }
 
     #endregion
