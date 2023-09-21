732e264f488a0e8c3c6c9254aa61dcee2cb8deac
kian <lordkian2000@gmail.com>
2023-09-21 21:26:17 +0200

    added PostToChannel and LoadSpeiseContainer to Operator

--- Mensa-Marburg/Operator.cs
+++ Mensa-Marburg/Operator.cs
@@ -14,9 +14,58 @@ public class Operator
     {
         Instance = new Operator();
     }
-    
+
     private Operator()
     {
-        
+    }
+
+    public void LoadSpeiseContainer()
+    {
+        var dateStr = DateTime.Now.ToString("yyyy.MM.dd.HH");
+        CurrentSpeiseContainer = new SpeiseContainer();
+        CurrentSpeiseContainer.DownloadData();
+        CurrentSpeiseContainer.DownloadTime = DateTime.Now;
+
+        var dirPath = Path.Combine(Setting.WorkDir, "logs");
+        if (!Directory.Exists(dirPath))
+            Directory.CreateDirectory(dirPath);
+        var sw = new StreamWriter(Path.Combine(dirPath,
+            "log." + dateStr + ".json"));
+        sw.WriteLine(JsonConvert.SerializeObject(CurrentSpeiseContainer));
+        sw.Close();
+    }
+
+    public void PostToChannel()
+    {
+        if (CurrentSpeiseContainer == null)
+            throw new Exception("SpeiseContainer is null");
+        if ((CurrentSpeiseContainer.DownloadTime - DateTime.Now).Hours > 0)
+            throw new Exception("SpeiseContainer is too old");
+
+        var today = DateTime.Today.ToString("yyyy-MM-dd");
+        var todayFoods = (
+            from s in CurrentSpeiseContainer.Gerichte
+            where s.Date == today
+            select s
+        ).ToList();
+
+        var text = "Today Menu\n";
+        foreach (var item in todayFoods)
+        {
+            text += $"{item.EssenType}: {item.Name} ({item.Kosten})\nKennzeichnungen: ";
+            foreach (var keypair in item.Kennzeichnungen)
+                text += keypair.Key + ") " + keypair.Value + " ";
+            text += "\n\n";
+        }
+
+        foreach (var item in CurrentSpeiseContainer.Beilagen)
+        {
+            text += $"{item.Type}: {item.Name}\nKennzeichnungen: ";
+            foreach (var keypair in item.Kennzeichnungen)
+                text += keypair.Key + ") " + keypair.Value + " ";
+            text += "\n\n";
+        }
+
+        Bot.PostToChannel(text);
     }
 }
