6ce50b15779f65f2b70040fbb6a71ce4bcc7f52b
kian <lordkian2000@gmail.com>
2023-09-21 22:08:33 +0200

    Added InitSetting to Program.cs

--- Mensa-Marburg/Program.cs
+++ Mensa-Marburg/Program.cs
@@ -1,11 +1,10 @@
-﻿using Mensa_Marburg;
-using Mensa_Marburg.Data;
+﻿using System.Text.RegularExpressions;
+using Mensa_Marburg;
 using Mensa_Marburg.Scheduler;
-using Newtonsoft.Json;
 
 Console.WriteLine("Starting");
 
-if (Environment.GetEnvironmentVariable("init") == "true" || 
+if (Environment.GetEnvironmentVariable("init") == "true" ||
     Environment.GetCommandLineArgs().Contains("--init"))
     InitSetting();
 else if (!Setting.LoadSetting())
@@ -13,19 +12,49 @@ else if (!Setting.LoadSetting())
     Setting.SetNewInstance();
     Setting.SaveSetting();
     Console.WriteLine("Setting File was created. Please fill it");
-    return;
 }
-else
+else if (Setting.Instance.IsSet)
 {
    
 }
 
 
-Console.WriteLine("fertig");
-Console.ReadKey();
-
 void InitSetting()
 {
+    var telegramBotTokenRegex = new Regex("/^[0-9]{8,10}:[a-zA-Z0-9_-]{35}$/");
+    var TelegramIDRegex = new Regex("^(-)?\\d{6,11}$");
     Setting.SetNewInstance();
-    
+
+    Console.WriteLine("Please Enter Telegram Bot Token");
+    var tmp = Console.ReadLine();
+    while (!telegramBotTokenRegex.IsMatch(tmp))
+    {
+        Console.WriteLine("Token is invalid.\nPlease Enter valid Telegram Bot Token");
+        tmp = Console.ReadLine();
+    }
+
+    Setting.Instance.TelegramBotToken = tmp;
+
+    Console.WriteLine("Please Enter Telegram user id of first Admin");
+    tmp = Console.ReadLine();
+    while (!TelegramIDRegex.IsMatch(tmp))
+    {
+        Console.WriteLine("user id is invalid.\nPlease Enter valid Telegram user id");
+        tmp = Console.ReadLine();
+    }
+
+    Setting.Instance.AdminsIDs.Add(long.Parse(tmp));
+
+    Console.WriteLine("Please Enter Telegram id of Channel");
+    tmp = Console.ReadLine();
+    while (!TelegramIDRegex.IsMatch(tmp))
+    {
+        Console.WriteLine("Channel id is invalid.\nPlease Enter valid Telegram Channel id");
+        tmp = Console.ReadLine();
+    }
+
+    Setting.Instance.ChannelID = long.Parse(tmp);
+    Setting.Instance.BaseURL = "https://studentenwerk-marburg.de/essen-trinken/speisekarte/";
+    Setting.Instance.IsSet = true;
+    Setting.SaveSetting();
 }
