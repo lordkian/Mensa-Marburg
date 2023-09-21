d76b8dc43fbe032ce2f42f8cd2c0d488b3174c60
kian <lordkian2000@gmail.com>
2023-09-21 21:52:39 +0200

    update Program

--- Mensa-Marburg/Program.cs
+++ Mensa-Marburg/Program.cs
@@ -1,11 +1,31 @@
 ﻿using Mensa_Marburg;
+using Mensa_Marburg.Data;
+using Mensa_Marburg.Scheduler;
+using Newtonsoft.Json;
 
 Console.WriteLine("Starting");
 
-if (!Setting.LoadSetting())
+if (Environment.GetEnvironmentVariable("init") == "true" || 
+    Environment.GetCommandLineArgs().Contains("--init"))
+    InitSetting();
+else if (!Setting.LoadSetting())
 {
+    Setting.SetNewInstance();
+    Setting.SaveSetting();
     Console.WriteLine("Setting File was created. Please fill it");
     return;
 }
+else
+{
+   
+}
 
+
+Console.WriteLine("fertig");
 Console.ReadKey();
+
+void InitSetting()
+{
+    Setting.SetNewInstance();
+    
+}
