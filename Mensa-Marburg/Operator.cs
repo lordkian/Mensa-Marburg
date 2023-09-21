7dea9c6e4b6343ca9c2e929c4d2d91bf02455b98
kian <lordkian2000@gmail.com>
2023-09-21 21:21:05 +0200

    Added TelegramBot and Scheduler Property to Operator

--- Mensa-Marburg/Operator.cs
+++ Mensa-Marburg/Operator.cs
@@ -1,10 +1,13 @@
 using Mensa_Marburg.Data;
+using Newtonsoft.Json;
 
 namespace Mensa_Marburg;
 
 public class Operator
 {
-    public SpeiseContainer SpeiseContainer { get; private set; }
+    public SpeiseContainer CurrentSpeiseContainer { get; private set; }
+    public TelegramBot Bot { get; set; }
+    public Scheduler.Scheduler Scheduler { get; set; }
     public static Operator Instance;
 
     static Operator()
